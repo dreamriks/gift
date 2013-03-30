b04b29be381edf047fd222b7e6848c1169f66d11
Rishi Tiwari <dreamriks@gmail.com>
2013-03-30 04:18:03 -0600

    added all folders from second image

---
+++ configuration.php
@@ -0,0 +1,92 @@
+<?php
+class JConfig {
+	var $generator_tag = '';
+	var $offline = '0';
+	var $editor = 'tinymce';
+	var $list_limit = '20';
+	var $helpurl = 'http://help.joomla.org';
+	var $debug = '0';
+	var $debug_lang = '0';
+	var $sef = '0';
+	var $sef_rewrite = '0';
+	var $sef_suffix = '0';
+	var $feed_limit = '10';
+	var $feed_email = 'author';
+	var $secret = 'vXaLjyXjqhRfTRiq';
+	var $gzip = '0';
+	var $error_reporting = '-1';
+	var $xmlrpc_server = '1';
+	var $log_path = '/home2/giftjaip/public_html/logs';
+	var $tmp_path = '/home2/giftjaip/public_html/tmp';
+	var $live_site = 'http://giftjaipur.com';
+	var $force_ssl = '0';
+	var $offset = '0';
+	var $caching = '0';
+	var $cachetime = '15';
+	var $cache_handler = 'file';
+	var $memcache_settings = array();
+	var $ftp_enable = '0';
+	var $ftp_host = '127.0.0.1';
+	var $ftp_port = '21';
+	var $ftp_user = '';
+	var $ftp_pass = '';
+	var $ftp_root = '';
+	var $dbtype = 'mysqli';
+	var $host = 'localhost';
+	var $user = 'giftjaip_naman';
+	var $db = 'giftjaip_962012';
+	var $dbprefix = 'jos_';
+	var $mailer = 'mail';
+	var $mailfrom = 'admin@giftjaipur.com';
+	var $fromname = 'GiftJaipur';
+	var $sendmail = '/usr/sbin/sendmail';
+	var $smtpauth = '1';
+	var $smtpsecure = 'ssl';
+	var $smtpport = '465';
+	var $smtpuser = 'namangarg88@gmail.com';
+	var $smtppass = 'natraj10';
+	var $smtphost = 'smtp.gmail.com';
+	var $html_title_config = '[SITENAME]';
+	var $html_title_default = '[SITENAME]';
+	var $use_global_metadesc = '1';
+	var $use_global_keywords = '1';
+	var $metarobots = '';
+	var $custom_default = '1';
+	var $metagenerator = '0';
+	var $MetaAuthor = '1';
+	var $MetaTitle = '1';
+	var $lifetime = '15';
+	var $session_handler = 'database';
+	var $password = 'boroplus88';
+	var $sitename = 'Send Anniversary gifts to Jaipur';
+	var $MetaDesc = 'We deliver sweets from branded shops of Jaipur, Eggless bakery cakes, Best and fresh flower of Jaipur, Jaipur local delivery, Diwali gifts, navratri gifts, Delivery within 2 hrs';
+	var $MetaKeys = 'Online gift shop Jaipur
+flower gift delivery
+roses to jaipur
+cakes to jaipur
+cakes online delivery
+designer cake
+eggless cakes to jaipur
+dry fruits
+badam
+kaju to jaipur
+pista
+pyaaz ki kachori
+ferrero rocher chocolate to jaipur
+birthday gifts to jaipur
+birthday flower delivery
+teady bear to jaipur
+kanha sweets to jaipur
+sweets to jaipur
+samosa
+kachori
+laddu 
+kaju katli to jaipur
+lilly flowers
+birthday cakes
+wedding gifts
+gifts for girls
+gift for boys';
+	var $offline_message = 'This site is down for maintenance. Please check back again soon.';
+}
+?>
